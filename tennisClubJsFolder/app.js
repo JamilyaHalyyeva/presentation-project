01a69b8c4a82f346ae1984276c2e1cd75e81b491
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2023-06-26 02:05:55 +0200

    navigation header, slider section, about section are done

---
+++ tennisClubJsFolder/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    //Slider
+    $('#heroslider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        items:1,
+        dots:false,
+        smartSpeed:1000,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+})
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar.fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > 700);
+    });
+  });
